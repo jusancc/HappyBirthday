92d3101196f3bb2c2d7565973ba26ea8a388c115
jusancc <jusancc@unirioja.es>
2024-02-16 11:36:41 +0100

    leccion 3 paso 7

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -30,16 +30,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message : String, modifier : Modifier = Modifier) {
+fun GreetingText(message : String,from : String , modifier : Modifier = Modifier) {
     Text(text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp)
+    Text(text = from,
+        fontSize = 36.sp)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!")
+        GreetingText(message = "Happy Birthday Sam!", from = "from Emma")
     }
 }
